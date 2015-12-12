a98ec4482de31b86b4b82b97c5efbe6f094c0e9c
Yu Li <YLiLarry@gmail.com>
2015-12-12 02:03:30 -0500

    Support for frame:v

--- src/FFmpeg/Data/H264.hs
+++ src/FFmpeg/Data/H264.hs
@@ -4,24 +4,35 @@ module FFmpeg.Data.H264 where
 
 import FFmpeg.Config
 import Data.SL
+import System.FilePath
 
 data H264 = H264 {
-        h264In  :: FilePath
-      , h264Out :: FilePath
-      , h264RV  :: String
-      , h264CRF :: String
+        input     :: FilePath
+      , output    :: FilePath
+      , frameRate :: String
+      , crf       :: String
+      , prefix    :: String
+      , suffix    :: String
+      , frames    :: Maybe String
    } deriving (Generic, Eq, Show)
 
 
 instance Config H264 where
    defaultCfg = H264 {
-           h264In  = ""
-         , h264Out = ""
-         , h264RV  = "25"
-         , h264CRF = "18"
+           input     = ""
+         , output    = ""
+         , frameRate = "25"
+         , crf    = "18"
+         , prefix = "h264_"
+         , suffix = ".mp4"
+         , frames = Nothing
       }
-   makeArgs conf = ["-i", h264In conf, "-codec:v", "libx264", "-crf", h264CRF conf, h264Out conf]
-   setIOFile a inp outp = a {h264In = inp, h264Out = outp}
+   makeArgs conf =
+      ["-i", input conf, "-codec:v", "libx264", "-crf", crf conf]
+      ++ maybe [] (\x -> ["-frames:v", x]) (frames conf)
+      ++ [output conf] -- output
+   setIOFile a inp _ = a {input = inp, output = outp}
+      where outp = takeDirectory inp ++ "/" ++ prefix a ++ takeBaseName inp ++ suffix a
 
 instance SL H264
 instance ToJSON H264
