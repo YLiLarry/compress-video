e86d045a9f5e2335ed5412b8488390c358931241
ylilarry <ylilarry@gmail.com>
2016-01-26 22:35:33 -0500

    Resolve pip overflow. Bugfixes.

--- src/FFmpeg/Probe.hs
+++ src/FFmpeg/Probe.hs
@@ -11,10 +11,12 @@ import System.Process
 import Text.Printf
 import System.IO
 import Data.List.Split
+import Data.Maybe
 
 data Probe = Probe {
    fpath        :: String,
    size         :: Int,
+   totalBitRate :: Int,
    codec        :: String,
    bitRate      :: Int,
    frames       :: Int,
@@ -27,28 +29,32 @@ data Probe = Probe {
 instance FromJSON Probe where
    parseJSON (Object o) = do
       let (Array streams) = o HM.! "streams"
-      let (Object video)  = V.head streams
-      let (Object audio)  = V.last streams
+      (Object video) <- maybe (fail "Cannot find a video stream.") return $ 
+         V.find (\(Object video) -> "video" == video HM.! "codec_type") streams
+      (Object audio) <- maybe (fail "Cannot find an audio stream.") return $ 
+         V.find (\(Object audio) -> "audio" == audio HM.! "codec_type") streams
       let (Object format) = o HM.! "format"
-      fpath'        <- format .: "filename"  
-      size'         <- format .: "size"
-      codec'        <- video  .: "codec_name"  
-      bitRate'      <- video  .: "bit_rate"
-      frames'       <- video  .: "nb_frames"
-      frameRate'    <- video  .: "avg_frame_rate"
-      height'       <- video  .: "height" 
-      width'        <- video  .: "width"
-      audioBitRate' <- audio  .: "bit_rate"
+      fpath'        <- format                 .: "filename"  
+      size'         <- read       <$> format  .: "size"
+      totalBitRate' <- read       <$> format  .: "bit_rate"
+      audioBitRate' <- read       <$> audio   .: "bit_rate"
+      bitRate'      <- (fmap read <$> video  .:? "bit_rate")      .!= (totalBitRate' - audioBitRate') 
+      codec'        <- video                  .: "codec_name"  
+      frames'       <- read       <$> video  .:? "nb_frames"      .!= "0"
+      frameRate'    <- video                  .: "avg_frame_rate"
+      height'       <- video                  .: "height" 
+      width'        <- video                  .: "width"
       return Probe {
          fpath        = fpath',
-         size         = read size',
+         size         = size',
          codec        = codec',
-         bitRate      = read bitRate' `div` 1000,
-         frames       = read frames',
+         totalBitRate = totalBitRate' `div` 1000,
+         bitRate      = bitRate' `div` 1000,
+         frames       = frames',
          frameRate    = readFrameRate frameRate',
          height       = height',
          width        = width',
-         audioBitRate = read audioBitRate' `div` 1000
+         audioBitRate = audioBitRate' `div` 1000
       }      
       where
          readFrameRate :: String -> Int
@@ -58,16 +64,21 @@ instance FromJSON Probe where
       
 ffprobe :: FilePath -> IO Probe
 ffprobe file = do 
-   (_,Just out,Just err,h) <- createProcess (proc ffprobeBin ["-v", "quiet", "-of", "json", "-show_format", "-show_streams", file]) {
-      std_out = CreatePipe,
-      std_err = CreatePipe
-   }
+   let args = ([]
+         ++ ["-v", "quiet"]
+         ++ ["-of", "json"]
+         ++ ["-show_entries", "format=filename,size,bit_rate:stream=codec_name,codec_type,bit_rate,nb_frames,avg_frame_rate,height,width"]
+         ++ [file])
+   (_,Just out,Just err,h) <- createProcess (proc ffprobeBin args) {
+         std_out = CreatePipe,
+         std_err = CreatePipe
+      }
    waitForProcess h
    s <- hGetContents err
    b <- B.hGetContents out
    let showError e = error $ printf 
-         "FFProbe Output:\n%s Error:\n %s\nProbe: cannot read the video info.\nDecode:\n%s\n"
-         s (show b) e
+         "FFProbe Output:\n%s Error: ffprobe %s\n %s\nProbe: Cannot read the video info.\nDecode: %s\n"
+         s (unwords args) (show b) e
    return $ either showError id $ eitherDecode b
             
             
