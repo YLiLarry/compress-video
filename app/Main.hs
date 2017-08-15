1204917beae84ba0f7b7122a9300b86e9bae4ff6
Yu Li <YLiLarry@gmail.com>
2017-08-14 21:26:56 -0700

    add a help hint

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,31 @@
 module Main where
 
-import Data.SL
-import FFmpeg.Config
-import FFmpeg.Probe
-import FFmpeg.Process
-import FFmpeg.Data.H264
-import System.Environment
-import Control.Monad (void)
-import System.FilePath
-import Data.Maybe
-import System.IO
+import           Control.Monad      (void)
+import           Data.Maybe
+import           Data.SL
+import           FFmpeg.Config
+import           FFmpeg.Data.H264
+import           FFmpeg.Probe
+import           FFmpeg.Process
+import           System.Environment
+import           System.FilePath
+import           System.IO
 
 main :: IO ()
 main = do
-   hSetBuffering stdout NoBuffering
-   [inPath, outPath, confPath] <- getArgs
-   conf <- loadCfg confPath
-   info <- ffprobe inPath
-   void $ ffmpeg conf info  
-   -- setIOFile conf inPath outPath
+    hSetBuffering stdout NoBuffering
+    args <- getArgs
+    case args of
+        [inPath, outPath, confPath] -> do
+            conf <- loadCfg confPath
+            info <- ffprobe inPath
+            void $ ffmpeg conf info
+        _ -> putStrLn "[input path] [output path] [config path]"
+    -- setIOFile conf inPath outPath
 
 
 loadCfg :: FilePath -> IO LoadedCfg
 loadCfg path =
-   case takeExtension path of
-      ".h264" -> LoadedCfg <$> (load path :: IO H264)
-      otherwise -> error $ "Undefined extension: " ++ show path
+    case takeExtension path of
+        ".h264" -> LoadedCfg <$> (load path :: IO H264)
+        _       -> error $ "Undefined extension: " ++ show path
